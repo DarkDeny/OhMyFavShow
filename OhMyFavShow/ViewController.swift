ab41c6385f9ec407926c436602e562900ea61471
Dmitry Denisov <darkdeny@gmail.com>
2022-01-23 15:32:48 +0300

    initial commit

---
+++ OhMyFavShow/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OhMyFavShow
+//
+//  Created by Dmitry Denisov on 23.01.2022.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
